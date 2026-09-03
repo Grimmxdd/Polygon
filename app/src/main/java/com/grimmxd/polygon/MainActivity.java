cfdbb6ebf309fe8b228b6de24988a6583d14d8fb
Grimxd <108762024+Grimmxdd@users.noreply.github.com>
2026-09-03 12:33:34 -0400

    Add files via upload

--- app/src/main/java/com/grimmxd/polygon/MainActivity.java
+++ app/src/main/java/com/grimmxd/polygon/MainActivity.java
@@ -24,10 +24,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        hideStatusBar();
-
         webView = new WebView(this);
         setContentView(webView);
+
+        hideStatusBar();
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -90,18 +90,21 @@ public class MainActivity extends Activity {
 
     private void hideStatusBar() {
         Window window = getWindow();
+
         if (android.os.Build.VERSION.SDK_INT >= 30) {
-            window.setDecorFitsSystemWindows(true);
-            window.getInsetsController().hide(
-                    android.view.WindowInsets.Type.statusBars());
+            android.view.WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.hide(android.view.WindowInsets.Type.statusBars());
+                controller.setSystemBarsBehavior(
+                        android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
         } else {
             window.setFlags(
                     WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         }
-
-        window.getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 }
